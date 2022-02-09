bc9d4623485d432084e94979cd5976e260b06d96
Stefano Proietti <stef.pro@hotmail.com>
2022-02-09 18:02:27 -0500

    hero section updated

--- src/Components/HeroSection/HeroSection.tsx
+++ src/Components/HeroSection/HeroSection.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+import { BsArrowDownCircle } from 'react-icons/bs';
 
 const PrimaryContainer = styled.div`
     display: flex;
@@ -13,12 +14,12 @@ const ContainerOne = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    padding: 20px 10px;
+    padding: 40px 0px;
 `
 
 const DevWord = styled.div`
     color: hsl(155, 70%, 49%);
-    font-size: 40px;
+    font-size: 45px;
     font-weight: bold;
 `
 
@@ -36,7 +37,63 @@ const SloganBox = styled.div`
 
 const Text = styled.p`
     color: white;
-    font-size: 50px;
+    font-size: 60px;
+    font-family: 'Oswald';
+`
+
+const GifBox = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50%;
+    padding-left: 20px;
+`
+
+/* Third Container for button and link icons */
+const ContainerThree = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    padding-top: 20px;
+`
+
+const NextSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+const bounce = keyframes`
+    10% {
+        height: 35px;
+        width: 35px;
+    }
+    30% {
+        height: 35px;
+        width: 35px;
+    }
+    50% {
+        height: 35px;
+        width: 35px;
+        transform: translateY(20px);
+    }
+    75% {
+        height: 35px;
+        width: 35px;
+    }
+    100% {
+        transform: translateY(0px);
+    }
+`
+
+const ButtonBox = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center
+    width: 50px;
+    height: 50%;
+    animation: ${bounce} 0.9s infinite
 `
 
 const HeroSection = () => {
@@ -49,7 +106,19 @@ const HeroSection = () => {
                 <SloganBox>
                     <Text>From making music to building web applications, I love letting my <strong style={{color: 'hsl(360, 50%, 60%)'}}>creativity</strong> go wild!</Text>
                 </SloganBox>
+                <GifBox>
+                    <img src={require("./../../Images/tablet-gif.gif")} alt="devices" style={{width: '475px', height: '475px'}}/>
+                    <img src={require("./../../Images/search-gif.gif")} alt="search" style={{width: '475px', height: '475px'}}/>
+                </GifBox>
             </ContainerTwo>
+            <ContainerThree>
+                <NextSection>
+                    <Text style={{fontSize: '35px'}}>Projects</Text>
+                    <ButtonBox >
+                        <BsArrowDownCircle style={{color: 'white', width: '35px', height: '35px', cursor: 'pointer'}} />
+                    </ButtonBox>
+                </NextSection>
+            </ContainerThree>
         </PrimaryContainer>
     )
 }
